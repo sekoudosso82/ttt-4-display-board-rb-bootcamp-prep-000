a9c35fe7b2ea88a30ce868a2fd612ce3578352de
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-25 05:14:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-def display_board 
-  puts "  |   |   "
+def display_board (board)
+  puts " #{[]} |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
