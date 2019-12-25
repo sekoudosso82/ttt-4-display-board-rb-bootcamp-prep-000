353e7a3c30d3cf21b7c3acfdb9961fe2a5893119
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-25 05:15:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 
 def display_board (board)
-  puts " #{[0]} | #{[1]}  | #{[2]}  "
+  puts " #{board[0]} | #{board[1]}  | #{board[2]}  "
   puts "-----------"
-  puts "  #{[3]} | #{[4]}  |  #{[5]} "
+  puts "  #{board[3]} | #{board[4]}  |  #{board[5]} "
   puts "-----------"
-  puts "  #{[6]} | #{[7]}  |  #{[8]} "
+  puts "  #{board[6]} | #{board[7]}  |  #{board[8]} "
 end
